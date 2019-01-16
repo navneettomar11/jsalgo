3e709933be80a4acf0bd776bfa86fae1831a7b22
Navneet Singh Tomar <navneet_tomar11@hotmail.com>
2019-01-16 23:25:39 +0530

    working on count of substring

--- js/strings/how-many-substring.js
+++ js/strings/how-many-substring.js
@@ -3,6 +3,9 @@ function countSubstrings(s, queries) {
 	queries.array.forEach(elem => {
 		let subStr = s.substring(elem[0], elem[1]);
 		let substrLength = subStr.length;
+		for(var i = 0 ; i < substrLength; i++){
+			
+		}
 	});
 
 }
