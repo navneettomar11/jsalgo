637a27b3bbe37c475cec8a995f886c0da07fa093
Navneet Singh Tomar <navneet_tomar11@hotmail.com>
2019-01-18 23:43:35 +0530

    declare create suffix array function and lcp

--- js/strings/how-many-substring.js
+++ js/strings/how-many-substring.js
@@ -1,11 +1,21 @@
+import { lookupService } from "dns";
+
 function countSubstrings(s, queries) {
 
 	queries.array.forEach(elem => {
 		let subStr = s.substring(elem[0], elem[1]);
 		let substrLength = subStr.length;
 		for(var i = 0 ; i < substrLength; i++){
-			
+
 		}
 	});
+
+}
+
+function createSuffixArray(){
+	
+}
+
+function lcp(){
 
 }
